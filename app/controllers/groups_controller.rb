ccf18d934848fd94370201fb863eb9ad7e06b789
孙文轲 <sunwenke2009@126.com>
2017-06-23 12:48:17 +0800

    create groups index

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,69 @@
+class GroupsController < ApplicationController
+
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+
+  def edit
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+
+  def update
+
+    find_group_and_check_permission
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    find_group_and_check_permission
+
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
+  end
+
+
+
+
+  private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+end
